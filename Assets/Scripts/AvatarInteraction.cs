bcb781a314c6bba403a291724b0d84daa810ba2b
RyanW <ywang13@risd.edu>
2017-10-07 20:04:02 -0400

    adding Unity Project Files

---
+++ Assets/Scripts/AvatarInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarInteraction : MonoBehaviour {
+	
+    public bool inPublic = false;
+	public bool inSocial = false;
+	public bool inPersonal = false;
+	public int comfortLevel = 50;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PublicInteract(){
+        Debug.Log("Public Space Interaction");
+    }
+
+	public void SocialInteract()
+	{
+		Debug.Log("Social Space Interaction");
+	}
+
+	public void PersonalInteract()
+	{
+		Debug.Log("Personal Space Interaction");
+	}
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public AvatarInteraction myAvatarInteractionScript;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Public")
+        {
+            myAvatarInteractionScript.inPublic = true;
+            Debug.Log("Public Space " + myAvatarInteractionScript.inPublic);
+            myAvatarInteractionScript.PublicInteract(); //where the interaction will go
+
+        }
+
+        if (other.tag == "Social")
+        {
+            myAvatarInteractionScript.inSocial = true;
+            Debug.Log("Social Space " + myAvatarInteractionScript.inSocial);
+
+        }
+
+        if (other.tag == "Personal")
+        {
+            myAvatarInteractionScript.inPersonal = true;
+            Debug.Log("Personal Space " + myAvatarInteractionScript.inPersonal);
+
+        }
+
+        if (other.tag == "Avatar")
+        {
+            Debug.Log("Don't Touch Me!");
+            myAvatarInteractionScript.comfortLevel = 0;
+            Debug.Log("Comfort Level: " + myAvatarInteractionScript.comfortLevel + ", You Lose");
+        }
+
+    }
+
+		void OnTriggerExit(Collider other)
+		{
+			if (other.tag == "Public")
+			{
+                myAvatarInteractionScript.inPublic = false;
+				Debug.Log("Public Space " + myAvatarInteractionScript.inPublic);
+				//myAvatarInteractionScript.PublicInteract(); //where the interaction will go
+
+			}
+
+			if (other.tag == "Social")
+			{
+                myAvatarInteractionScript.inSocial = false;
+				Debug.Log("Social Space " + myAvatarInteractionScript.inSocial);
+
+			}
+
+			if (other.tag == "Personal")
+			{
+                myAvatarInteractionScript.inPersonal = false;
+				Debug.Log("Personal Space " + myAvatarInteractionScript.inPersonal);
+
+			}
+
+			if (other.tag == "Avatar")
+			{
+				Debug.Log("Don't Touch Me!");
+				myAvatarInteractionScript.comfortLevel = 0;
+				Debug.Log("Comfort Level: " + myAvatarInteractionScript.comfortLevel + ", You Lose");
+			}
+
+    
+    }
+}
